3155da24acee6a208adba1f257e7b019bae1f1b3
Abel Amuta <amutaprincedavid@gmail.com>
2024-03-20 01:55:27 +0100

    initial commit

--- script.js
+++ script.js
@@ -2,45 +2,6 @@ const number = document.getElementById("number");
 const convertBtn = document.getElementById("convert-btn");
 const output = document.getElementById("output");
 
-/*const convertToRoman = (num) => {
-    const numerals={
-        1: "I", 4: "IV", 5: "V", 9: "IX", 10: "X", 40: "XL", 50: "L", 90: "XC", 100: "C", 400: "CD", 500: "D", 900: "CM", 1000: "M"
-    };
-
-    let romanizedNums = "";
-    const arabicNums = Object.keys(numerals).reverse();
-
-    arabicNums.forEach(key => {
-        while (key <= num) {
-            romanizedNums += numerals[key];
-            num -= key;
-        }
-    });
-    return romanizedNums;
-}*/
-
-/*convertBtn.addEventListener("click", (num) => {
-    const numerals={
-        1: "I", 4: "IV", 5: "V", 9: "IX", 10: "X", 40: "XL", 50: "L", 90: "XC", 100: "C", 400: "CD", 500: "D", 900: "CM", 1000: "M"
-    };
-
-    let romanizedNums = "";
-    const arabicNums = Object.keys(numerals).reverse();
-    num = number.value;
-
-    if (!num) {
-        output.textContent = "Please enter a valid number";
-    }else{
-        arabicNums.forEach((key) => {
-            while (key <= num) {
-                romanizedNums += numerals[key];
-                num -= key;
-            }
-        });
-        output.textContent = romanizedNums;
-    }
-});*/
-
 convertBtn.addEventListener("click", (num) => {
     const numerals= [
         ["M" , 1000], ["CM" , 900], ["D" , 500], ["CD" , 400], ["C" , 100], ["XC" , 90], ["L" , 50], ["XL" , 40], ["X" , 10], ["IX" , 9], ["V" , 5], ["IV" , 4], ["I" , 1]
